e3bace66da28309cea42a8fb624fe0828b9ea42d
Miguel <miguel@MacBook.local>
2021-05-27 22:31:45 +0200

    Añadido el funcionamiento básico de la calculadora

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
-console.log("Actividad 4")
+let operando1;
+let operando2;
+let operador;
+let limpiarPantalla = false;
+
+let resultado;
+
+const operacion = (op) => {
+    if (document.getElementById('pantalla').value.length !== 0 && document.getElementById('pantalla').value !== '-E-') {
+        operando1 = document.getElementById('pantalla').value;
+        document.getElementById('pantalla').value += op;
+        operador = op;
+        limpiarPantalla = true;
+        if (op === 'sqrt') {
+            realizarOperacion();
+        }
+    }
+};
+
+const entradaNumero = (button) => {
+  if (!limpiarPantalla && document.getElementById('pantalla').value !== '-E-') {
+      document.getElementById('pantalla').value += button;
+  } else {
+      document.getElementById('pantalla').value = button;
+      limpiarPantalla = false;
+  }
+};
+
+const realizarOperacion = () => {
+  operando2 = document.getElementById('pantalla').value;
+  switch (operador) {
+      case '+':
+          resultado = suma(operando1, operando2);
+          break;
+      case '-':
+          resultado = resta(operando1, operando2);
+          break;
+      case '*':
+          resultado = multiplicacion(operando1, operando2);
+          break;
+      case '/':
+          if (Number(operando2) !== 0) {
+              resultado = division(operando1, operando2);
+          } else {
+              resultado = '-E-';
+              limpiarPantalla = true;
+          }
+          break;
+      case 'sqrt':
+          resultado = raizCuadrada(operando1);
+          break;
+  }
+  document.getElementById('pantalla').value = resultado;
+  limpiarPantalla = true;
+};
+
+const limpiarTodo = () => {
+  operando1 = 0;
+  operando2 = 0;
+  operador = '';
+  limpiarPantalla = false;
+  resultado = 0;
+  document.getElementById('pantalla').value = '';
+};
